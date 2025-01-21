044eb9a5bae3053c764bfd8ef385bd66b004ffe9
Pyae Zin Phyo <pyaezinphyo@Patt-Bay-Bone-Jyi-2.local>
2025-01-21 11:08:33 +0700

    Vaniverse

---
+++ header.php
@@ -0,0 +1,51 @@
+<div class="logoDiv">
+    <a href="index.php"><img src="image/AU-logo-txt-black.png" alt="" class="logo"></a>
+    <!-- <span class="name">Badda Station</span> -->
+</div>
+<?php
+  include 'navbar.php';
+?>
+<!-- Slide SHow -->
+
+<div id="carouselExampleCaptions" class="carousel slide mt-3 mb-3">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+      <img src="image/a4.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block border slide-text">
+        <h5>Welcome To Vaniverse</h5>
+        <p class="">Your journey, your van, your way – <strong>book with ease today!</strong></p>
+        <button class="book-button">Book Your Adventure <i class="bi bi-search"></i></button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/a2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block border border-primary-subtle slide-text">
+        <h5>Welcome To Vaniverse</h5>
+        <p class="">Your journey, your van, your way – <strong>book with ease today!</strong></p>
+        <button class="book-button">Book Your Adventure <i class="bi bi-search"></i></button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image/a3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block border border-primary-subtle slide-text">
+        <h5>Welcome To Vaniverse</h5>
+        <p class="">Your journey, your van, your way – <strong>book with ease today!</strong></p>
+        <button class="book-button">Book Your Adventure <i class="bi bi-search"></i></button>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
